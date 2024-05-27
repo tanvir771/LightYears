fa9db9a4d05ce3cb5c706afa006f757d16b9d234
tanvir771 <tanvir.leon53@gmail.com>
2024-05-27 01:24:03 -0700

    cmake instructions for library files (dll)

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+#include <SFML/Graphics.hpp>
 
 int main() {
+	sf::RenderWindow renderWindow{ sf::VideoMode(100,100), "MyWindow" };
+
+	while (renderWindow.isOpen()) {
+		sf::Event windowEvent;
+		while (renderWindow.pollEvent(windowEvent)) {
+			if (windowEvent.type == sf::Event::EventType::Closed) {
+				renderWindow.close();
+			}
+		}
+	}
+
 	std::cout << "Hello, world" << std::endl;
 }
+
